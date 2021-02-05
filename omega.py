a7b2035f7508b441b3e3bd7ed06fc2dba2930859
jumdtw <ttnmrm1m3k5g1m3r3k@gmail.com>
2021-02-06 07:28:38 +0900

    add color debug

--- omega.py
+++ omega.py
@@ -1,6 +1,7 @@
 import tetris
 import copy
 import random
+import os
 
 class Omegaplayer:
 
@@ -254,6 +255,7 @@ class Omegaplayer:
 
     
     def debug_emap_print(self, maplist, index):
+        os.system('cls')
         x = maplist[index][0]
         y = maplist[index][1]
         ang = maplist[index][2]
@@ -263,18 +265,38 @@ class Omegaplayer:
                 if x+fx >= 0 and x+fx <= 11 and y+fy <= 20:
                     buffield[y+fy][x+fx] = buffield[y+fy][x+fx] or self.omegamino.minodate[self.omegamino.minotype[0]][ang][fy][fx]  
         for q in buffield:
-            print(q)
+            for p in q:
+                if p==0:
+                    print(f"{Color.WHITE}"+str(p)+' ', end='')
+                elif p==1:
+                    print(f"{Color.PURPLE}"+str(p)+' ', end='')
+                elif p==2:
+                    print(f"{Color.SKYBLUE}"+str(p)+' ', end='')
+                elif p==3:
+                    print(f"{Color.YELLOW}"+str(p)+' ', end='')
+                elif p==4:
+                    print(f"{Color.ORANGE}"+str(p)+' ', end='')
+                elif p==5:
+                    print(f"{Color.BLUE}"+str(p)+' ', end='')
+                elif p==6:
+                    print(f"{Color.YELLOWGREEN}"+str(p)+' ', end='')
+                elif p==7:
+                    print(f"{Color.RED}"+str(p)+' ', end='')
+            print(f"{Color.RESET}")
+
 
     
     def evaulate_map(self):
         self.calcflag = True
         maplist = self.search_map()
-        #self.debug_emap_print(maplist, 0)
+        mr = random.randint(0,len(maplist)-1)
+        self.debug_emap_print(maplist, mr)
         self.posminolist.pop(0)
-        self.posminolist.append(maplist[random.randint(0,len(maplist)-1)])
+        self.posminolist.append(maplist[mr])
         self.calcflag = False
 
 
+    # 同じ座標にいたら違う行動をとるように修正が必要
     def return_ctllist(self, curx, cury, curang):
         self.calcflag = True
         key = tetris.KEY_LEFT
@@ -327,3 +349,31 @@ class Omegaplayer:
         self.calcflag = False
         return key
 
+class Color:
+    BLACK          = '\033[30m'#(文字)黒
+    RED            = '\033[31m'#(文字)赤
+    GREEN          = '\033[32m'#(文字)緑
+    YELLOW         = '\033[33m'#(文字)黄
+    YELLOWGREEN    = '\033[38;2;171;207;0m'
+    BLUE           = '\033[34m'#(文字)青
+    SKYBLUE        = '\033[38;2;102;204;255m'#(文字)skyblue
+    MAGENTA        = '\033[35m'#(文字)マゼンタ
+    CYAN           = '\033[36m'#(文字)シアン
+    WHITE          = '\033[37m'#(文字)白
+    PURPLE         = '\033[38;2;255;0;255m' #(文字)紫
+    ORANGE         = '\033[38;2;255;165;0m' #(文字)オレンジ
+    COLOR_DEFAULT  = '\033[39m'#文字色をデフォルトに戻す
+    BOLD           = '\033[1m'#太字
+    UNDERLINE      = '\033[4m'#下線
+    INVISIBLE      = '\033[08m'#不可視
+    REVERCE        = '\033[07m'#文字色と背景色を反転
+    BG_BLACK       = '\033[40m'#(背景)黒
+    BG_RED         = '\033[41m'#(背景)赤
+    BG_GREEN       = '\033[42m'#(背景)緑
+    BG_YELLOW      = '\033[43m'#(背景)黄
+    BG_BLUE        = '\033[44m'#(背景)青
+    BG_MAGENTA     = '\033[45m'#(背景)マゼンタ
+    BG_CYAN        = '\033[46m'#(背景)シアン
+    BG_WHITE       = '\033[47m'#(背景)白
+    BG_DEFAULT     = '\033[49m'#背景色をデフォルトに戻す
+    RESET          = '\033[0m'#全てリセット
